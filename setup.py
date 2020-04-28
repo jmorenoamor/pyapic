e78168db2d1924cd9e3c4d8bb00d628635f0994e
Jesus Moreno Amor <jesus@morenoamor.com>
2020-04-28 15:13:46 +0200

    Test integration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyapic",
-    version="0.0.1",
+    version="0.0.2",
     author="Jesús Moreno Amor",
     author_email="jesus@morenoamor.com",
   	description='API Connect 2018 Management API',
